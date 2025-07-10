46f6aa6053a2403feccadaf6d70150b99ca7702c
Mihail538 <misankinm5@gmail.com>
2025-07-10 12:22:47 +0200

    First commit

---
+++ Perimeter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (){
+    unsigned long int a, b, x;
+    cout << "Side_a: ";
+    cin >> a;
+    cout << "Side_b: ";
+    cin >> b;
+    x = (a+b)*2;
+    cout << "Perimeter: " << x << endl;
+    return 0;
+}
