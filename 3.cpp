c9c930b4f45349a5a93b6bf2754d12e7ab1d6fb7
Mihail538 <misankinm5@gmail.com>
2025-07-10 13:22:12 +0200

    Third commit

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    unsigned short int x, y;
+    cout << "x: ";
+    cin >> x;
+    y = (4*x);
+    cout << "y: " << y << endl;
+    return 0;
+}
